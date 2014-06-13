0004536face82773fac471eb60204fc53cb66d76
StefanMihaylov <mihaylov.s@gmail.com>
2014-06-13 16:30:54 +0300

    add handlebars homework

---
+++ 08 JavaScript-UI-and-DOM/11b HTML-Templates-homework/HTML-Templates/dropdown-menu.js
@@ -0,0 +1,36 @@
+﻿window.onload = function () {
+    var list = [{
+        value: 1,
+        text: 'One'
+    }, {
+        value: 2,
+        text: 'Two'
+    }, {
+        value: 3,
+        text: 'Three'
+    }, {
+        value: 4,
+        text: 'Four'
+    }, {
+        value: 5,
+        text: 'Five'
+    }, {
+        value: 6,
+        text: 'Six'
+    }, {
+        value: 7,
+        text: 'Seven'
+    }, {
+        value: 8,
+        text: 'Eight'
+    }, {
+        value: 9,
+        text: 'Nine'
+    }];
+
+    var dropdownTemplatehtml = document.getElementById('dropdown-template').innerHTML;
+    var dropdownTemplate = Handlebars.compile(dropdownTemplatehtml);
+    document.getElementById('wrapper').innerHTML = dropdownTemplate({
+        list: list
+    });
+}
---
+++ 08 JavaScript-UI-and-DOM/11b HTML-Templates-homework/HTML-Templates/table-data.js
@@ -0,0 +1,73 @@
+﻿window.onload = function () {
+    var table = [{
+        title: "Course introduction",
+        firstDate: DateToString(new Date(2014, 05, 28, 18, 00)),
+        secondDate: DateToString(new Date(2014, 05, 29, 14, 00))
+    }, {
+        title: "Document Object Model",
+        firstDate: DateToString(new Date(2014, 05, 28, 18, 00)),
+        secondDate: DateToString(new Date(2014, 05, 29, 14, 00))
+    }, {
+        title: "HTML Canvas",
+        firstDate: DateToString(new Date(2014, 05, 29, 18, 00)),
+        secondDate: DateToString(new Date(2014, 05, 30, 14, 00))
+    }, {
+        title: "Kinetic JS Overview",
+        firstDate: DateToString(new Date(2014, 05, 29, 18, 00)),
+        secondDate: DateToString(new Date(2014, 05, 30, 14, 00))
+    }, {
+        title: "SVG and RafaelJS",
+        firstDate: DateToString(new Date(2014, 05, 04, 18, 00)),
+        secondDate: DateToString(new Date(2014, 05, 05, 14, 00))
+    }, {
+        title: "Animations with Canvas and SVG",
+        firstDate: DateToString(new Date(2014, 05, 04, 18, 00)),
+        secondDate: DateToString(new Date(2014, 05, 05, 14, 00))
+    }, {
+        title: "DOM operations",
+        firstDate: DateToString(new Date(2014, 05, 05, 18, 00)),
+        secondDate: DateToString(new Date(2014, 05, 06, 14, 00))
+    }, {
+        title: "Event model",
+        firstDate: DateToString(new Date(2014, 05, 05, 18, 00)),
+        secondDate: DateToString(new Date(2014, 05, 06, 14, 00))
+    }, {
+        title: "jQuery overview",
+        firstDate: DateToString(new Date(2014, 05, 11, 18, 00)),
+        secondDate: DateToString(new Date(2014, 05, 12, 14, 00))
+    }, {
+        title: "HTML Templates",
+        firstDate: DateToString(new Date(2014, 05, 11, 18, 00)),
+        secondDate: DateToString(new Date(2014, 05, 12, 14, 00))
+    }, {
+        title: "Exam preparation",
+        firstDate: DateToString(new Date(2014, 05, 12, 18, 00)),
+        secondDate: DateToString(new Date(2014, 05, 13, 14, 00))
+    }, {
+        title: "Exam",
+        firstDate: DateToString(new Date(2014, 05, 17, 10, 00)),
+        secondDate: DateToString(new Date(2014, 05, 17, 16, 30))
+    }, {
+        title: "Teamwork Defense",
+        firstDate: DateToString(new Date(2014, 05, 19, 10, 00)),
+        secondDate: DateToString(new Date(2014, 05, 19, 10, 00))
+    }];
+
+    var tableTemplateHtml = document.getElementById('table-template').innerHTML;
+    var tableTemplate = Handlebars.compile(tableTemplateHtml);
+    document.getElementById('wrapper').innerHTML = tableTemplate({
+        table: table
+    });
+
+    function DateToString(date) {
+        var daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+        return daysOfWeek[date.getDay()] + ' ' + padString(date.getHours()) + ':' + padString(date.getMinutes()) + ', ' +
+           padString(date.getDate()) + '-' + months[date.getMonth()] + '-' + date.getFullYear();
+    }
+
+    function padString(number) {
+        return (number < 10) ? "0" + number : "" + number;
+    }
+}
