de88141a3ba45de57fd713a23639da5a8c7725ca
StefanMihaylov <mihaylov.s@gmail.com>
2014-06-15 23:02:55 +0300

    add jquery plugin homework

---
+++ 08 JavaScript-UI-and-DOM/10b jQuery-Plugins-homework/jQuery-Plugins/scripts/dropdown.js
@@ -0,0 +1,66 @@
+﻿/// <reference path="../libs/jquery-2.1.1.min.js" />
+(function ($) {
+    $.fn.dropdown = function () {
+        var $this = this;
+        $this.hide();
+
+        var $divElement = $('<div/>')
+            .addClass('dropdown-list-container')
+            .insertAfter($this);
+        var $ulElement = $('<ul/>')
+            .addClass('dropdown-list-options')
+            .appendTo($divElement);
+
+        $('<img>').attr('src', 'imgs/arrow-big-down-512.png')
+            .addClass('dropdown-list-arrow')
+            .attr('width', '15px')
+            .attr('height', '20px')
+            .appendTo($divElement)
+            .on('click', openDropdown);
+
+        var $selectChildren = $this.children();
+        for (var i = 0; i < $selectChildren.length; i++) {
+            var $currentOption = $($selectChildren[i]);
+            $('<li/>')
+                .attr('data-value', $currentOption.val() - 1)
+                .addClass('list-item')
+                .text($currentOption.text())
+                .appendTo($ulElement);
+        }
+
+        var $listItems = $ulElement.children('.list-item').hide();;
+        var $firstLi = $listItems.first();
+        var $selectedLi = $firstLi.clone(true)
+                        .addClass('dropdown-list-options')
+                        .addClass('selected')
+                        .removeClass('list-item')
+                        .show()
+                        .insertBefore($firstLi)
+                        .on('click', openDropdown);
+
+        $listItems.on('click', function () {
+            var $this = $(this);
+            $listItems.removeClass('selected').hide();
+            $this.addClass('selected');
+            $selectedLi.attr('data-value', $this.attr('data-value'))
+                    .text($this.text());
+
+            $selectChildren.attr('selected', false);
+            var value = parseInt($this.attr('data-value')) + 1;
+            $selectChildren.filter('[value =' + value + ']').attr('selected', true);
+        });
+
+        $firstLi.click();
+
+        function openDropdown() {
+            if ($listItems.filter(':visible').length) {
+                $listItems.hide();
+            }
+            else {
+                $listItems.show();
+            }
+        }
+
+        return $this;
+    }
+})(jQuery)
---
+++ 08 JavaScript-UI-and-DOM/10b jQuery-Plugins-homework/jQuery-Plugins/scripts/message-box.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="../libs/jquery-2.1.1.min.js" />
+(function ($) {
+    $.fn.messageBox = function () {
+        var $this = $(this).hide();
+
+        return {
+            success: function (text) {
+                if ($this.filter(':visible').length === 0) {
+                    $this.css('background-color', 'green')
+                        .css('color', 'black')
+                        .text(text)
+                        .fadeIn(1000)
+                        .delay(3000)
+                        .fadeOut(1000);
+                }
+                return $this;
+
+            },
+            error: function (text) {
+                if ($this.filter(':visible').length === 0) {
+                    $this.css('background-color', 'red')
+                        .css('color', 'yellow')
+                        .text(text)
+                        .fadeIn(1000)
+                        .delay(3000)
+                        .fadeOut(1000);
+                }
+                return $this;
+            }
+        }
+    }
+})(jQuery);
