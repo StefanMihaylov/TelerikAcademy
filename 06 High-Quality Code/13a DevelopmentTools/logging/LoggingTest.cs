3410c5308ca27022ae7f0a2b031636273902b18d
StefanMihaylov <mihaylov.s@gmail.com>
2014-06-29 13:53:54 +0300

    add developer tools homework

---
+++ 06 High-Quality Code/13a DevelopmentTools/logging/LoggingTest.cs
@@ -0,0 +1,23 @@
+﻿namespace logging
+{
+    using log4net;
+    using log4net.Config;
+
+    public class LoggingTest
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(LoggingTest));
+
+        public static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+
+            log.Info("Entering application.");
+
+            log.Debug("Debug message");
+            log.Error("Error message");
+
+            log.Info("Exiting application.");
+        }
+    }
+}
+
